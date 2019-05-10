21b6b4db9b86f4c671257d5d366eee5e9b21757b
Ivan Solovev <ivan@solovev.one>
2019-05-10 15:48:40 +0300

    Init commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# storeon-cross-tab
+
+<img src="https://storeon.github.io/storeon/logo.svg" align="right"
+     alt="Storeon logo by Anton Lovchikov" width="160" height="142">
+
+## LICENSE
+
+MIT
---
+++ index.js
@@ -0,0 +1,33 @@
+/**
+ * Storeon module to update state at different browser tabs
+ * @param {Object} config The config object
+ * @param {String} [config.key = 'storeon-crosstab'] The default key
+ * @param {Boolean} [config.skipInit = true] Skip init event
+ * @return {Function} Storeon callback module
+ */
+var crossTab = function (config) {
+  config = config || {}
+
+  var key = config.key || 'storeon-crosstab'
+  var skipInit = config.skipInit || true
+
+  return function (store) {
+    store.on('crosstab', function (_, state) {
+      return state
+    })
+
+    store.on('@changed', function (state, event) {
+      if (skipInit) return skipInit = false
+
+      try {
+        localStorage.setItem(key, JSON.stringify(state))
+      } catch (e) {}
+    })
+
+    window.addEventListener('storage', function (event) {
+      store.dispatch('crosstab', JSON.parse(event.newValue))
+    })
+  }
+}
+
+module.exports = crossTab
