26bdd85376900bb20799d82bb3f144312861a03b
Ivan Solovev <ivan@solovev.one>
2019-05-11 18:03:45 +0300

    Add example

---
+++ example/index.js
@@ -0,0 +1,58 @@
+var createStore = require('storeon')
+
+var crossTab = require('../')
+
+// Initial state, reducers and business logic are packed in independent modules
+function increment (store) {
+  store.on('@init', function () {
+    return { count: 0, trim: true }
+  })
+
+  store.on('inc', function (state) {
+    return { count: state.count + 1 }
+  })
+
+  store.on('dec', function (state) {
+    return { count: state.count - 1 }
+  })
+
+  store.on('ten', function (state) {
+    return { count: state.count + 10 }
+  })
+}
+
+// Filter callback
+function filter (event) {
+  return event !== 'ten'
+}
+
+// Initialization crossTab
+var syncTab = crossTab({ filter: filter })
+
+// Create store
+var store = createStore([increment, syncTab])
+
+var counter = document.querySelector('.counter')
+counter.innerText = store.get().count
+
+store.on('@changed', function (state) {
+  counter.innerText = state.count
+})
+
+document
+  .querySelector('.inc')
+  .addEventListener('click', function () {
+    store.dispatch('inc', { type: 'inc-world' })
+  })
+
+document
+  .querySelector('.dec')
+  .addEventListener('click', function () {
+    store.dispatch('dec', { type: 'dec-world' })
+  })
+
+document
+  .querySelector('.ten')
+  .addEventListener('click', function () {
+    store.dispatch('ten')
+  })
